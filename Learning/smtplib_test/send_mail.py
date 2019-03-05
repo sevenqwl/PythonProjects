84c5dc94ffe8408b7dbdd99ec51ac35e6dff116f
sevenqwl <sevenqwl@163.com>
2019-03-05 11:04:52 +0800

    Learning Change

--- Learning/smtplib_test/send_mail.py
+++ Learning/smtplib_test/send_mail.py
@@ -10,10 +10,9 @@ from email.mime.multipart import MIMEMultipart
 HOST = 'smtp.163.com'
 PORT = '25'  # 端口
 
-FROM = 'sevenqwl@163.com'
+FROM = 'xxx@163.com'
 password = 'xxx'
-TO = 'sevenqwl@163.com;154308618@qq.com'
-# TO = 'qiwenlong@jd.com'
+TO = 'xxx@163.com;xxx@qq.com'
 
 def message_plain_html(TYPE, CONTENT, SUBJECT, FROM, TO):
     message = MIMEText(CONTENT, TYPE, 'utf-8')
